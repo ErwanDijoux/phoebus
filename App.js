0fca092851a288d97224fc9dcbecb3e9da5c7056
Gunfood 88 <aymen.ganfoud88@gmail.com>
2019-05-29 11:05:50 +0200

    createLibrary

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {Font} from 'expo';
 import Connexion from './Components/Screens/Connexion';
+import LibraryScreen from './Components/Screens/LibraryScreen';
 
 
 
 export default class App extends React.Component {
+  state = {
+    loaded: false,
+  };
+
+  componentWillMount() {
+    this._loadAssetsAsync();
+  }
+  _loadAssetsAsync = async () => {
+    await Font.loadAsync({
+      openSans: require('./assets/fonts/OpenSans-Regular.ttf'),
+      openBold: require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+    this.setState({ loaded: true });
+  };
+
   render() {
     return (
       <View style={{flex:1}}>
-        <Connexion/>
+      {  this.state.loaded ? (
+      <LibraryScreen/>
+    ) : null}
       </View>
     );
   }
